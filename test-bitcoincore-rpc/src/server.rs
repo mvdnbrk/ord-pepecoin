53560a59e63e600dbecca0032c07c7c9c2ae39a1
Mark van den Broek <mvdnbrk@protonmail.com>
2026-03-07 10:00:42 +0100

    fix: use assert! instead of assert_eq! with literal bool

--- test-bitcoincore-rpc/src/server.rs
+++ test-bitcoincore-rpc/src/server.rs
@@ -142,7 +142,7 @@ impl Api for Server {
   }
 
   fn get_block(&self, block_hash: BlockHash, verbose: bool) -> Result<String, jsonrpc_core::Error> {
-    assert_eq!(verbose, false, "Verbosity level {verbose} is unsupported");
+    assert!(!verbose, "Verbosity level {verbose} is unsupported");
     match self.state().blocks.get(&block_hash) {
       Some(block) => Ok(hex::encode(serialize(block))),
       None => Err(Self::not_found()),
